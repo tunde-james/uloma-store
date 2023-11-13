690be5e71c7d5115e12414a31ba70ebfd05cf2db
Tunde James <tundejames04@gmail.com>
2023-11-13 16:28:31 +0100

    Create product, category, and heroImage schema in sanity
    Create the sanity client

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import category from './category'
+import heroImages from './hero-images'
+import product from './product'
+
+export const schemaTypes = [product, category, heroImages]
